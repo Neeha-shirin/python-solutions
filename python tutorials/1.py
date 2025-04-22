348cb57f27c922f779bb5f9a38061710b80ff223
Neeha-shirin <neehasherin987@gmail.com>
2025-04-22 07:38:54 +0530

    new

---
+++ python tutorials/1.py
@@ -0,0 +1,11 @@
+# cheak whather the number is odd or even
+     
+def oddeven(num):
+    if num % 2 ==0:
+        print("even")
+
+    else:
+        print("odd")
+
+n=int(input("enter the number"))
+oddeven(n)
---
+++ python tutorials/2.py
@@ -0,0 +1,9 @@
+# reverse the string 
+def rev(s):
+    return s[::-1]
+
+nrev= "neeha"
+result=rev(nrev)
+
+print(nrev)
+print(result)
